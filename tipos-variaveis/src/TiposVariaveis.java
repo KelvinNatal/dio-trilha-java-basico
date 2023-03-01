1193501562235b6266d2c63724fee292d800a239
KelvinNatal <kelvinnatal@hotmail.com>
2023-03-01 20:39:22 -0300

    Metodos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 10;
+        int numero2 = -25;
+        
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+
+        if (numero < 0 || numero2 < 0)
+            System.out.println("Número negativo");
+        else
+            System.out.println("Número Positivo");
     }
-}
+}   
